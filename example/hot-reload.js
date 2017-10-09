8d86975efdb9b1cfa598fcfef10be56ab163d30e
Xavier Gouley <xavier.gouley@prestashop.com>
2017-10-09 15:24:19 +0200

    try to make it work...

--- example/hot-reload.js
+++ example/hot-reload.js
@@ -5,8 +5,10 @@ require('colors')
 
 const server = require('../lib').server
 
-// server.use(require('../lib/plugins/ip-cam'))
 server.use(require('../lib/plugins/developer-tools'))
+
+// TODO !0: if env exists, split by ',', then forEach: server.use(require(...))
+console.log('PPPPPPPPP', process.env.ASTERISM_PLUGINS)
 
 server.start(8090, ['127.0.0.1', '0.0.0.0', '::1'], function () {
   console.log('Hot-reload mode Asterism listening on port 8090!'.cyan)
